d8c22b523fc4168d588c6d85efa6f4eb3a5bcbad
EC2 Default User <ec2-user@ip-172-31-36-127.ec2.internal>
2021-09-15 08:20:56 +0000

    order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def new
+    @customer = current_customer
+    @order = Order.new
+  end
+
+  def confirm
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def thanks
+
+  end
 end
